99717c7b805a2a815e79ee7b3b67d38b87d8f379
Victor Gonçalves <victor@goncalvesmarques.com>
2025-03-08 21:41:50 -0300

    ci: change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,15 @@
-# Etapa de build
-FROM node:22.14.0 AS builder
-WORKDIR /app
-COPY ["package*.json","tsconfig.json", "tsup.config.ts","./"]
-
-COPY src ./src
-# ENV NODE_TLS_REJECT_UNAUTHORIZED=0
-
-# ENV NODE_ENV=prod
-# ENV APP_PORT='3000'
-RUN npm install
-RUN npm run build
-# RUN npm run doc:generate-build
-
-# Etapa de produção
-FROM node:22.14.0-slim
+FROM golang:1.23.5 AS builder
 WORKDIR /app
 
-# COPY --from=builder /app/package*.json ./
-COPY ["package*.json","./"]
-# ENV NODE_TLS_REJECT_UNAUTHORIZED=0
+COPY go.mod ./
+RUN go mod download
 
+COPY server.go ./
 
-# ENV NODE_ENV=prod
-# ENV APP_PORT='3000'
-RUN npm install
-COPY --from=builder /app/build ./build
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server .
 
-USER node
+FROM scratch
+WORKDIR /app
+COPY --from=builder /app/server .
 
-EXPOSE 3000
-
-CMD ["npm", "start"]
+ENTRYPOINT ["./server"]
--- server.go
+++ server.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
-	// "os"
+	"time"
 )
 
 func main() {
+	fmt.Println("Server is running on port 8080")
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/ap1/v1/doctor", Doctor)
 	http.ListenAndServe(":8080", nil)
+
+}
+
+var startedAt = time.Now()
+
+func Doctor(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Doctor"))
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	// name := os.Getenv("NAME")
-	// age := os.Getenv("AGE")
-	w.Write([]byte("<h1>Hello, Jovem</h1>"))
-	// fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
+	w.Write([]byte("<h1>Hello, DOg</h1>"))
 }
+
+// NOTE: Versão para o HPA
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+}
+
+// func Healthz(w http.ResponseWriter, r *http.Request) {
+// 	duration := time.Since(startedAt)
+
+// 	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+// 		w.WriteHeader(500)
+// 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+// 	} else {
+// 		w.WriteHeader(200)
+// 		w.Write([]byte("ok"))
+// 	}
+// }
